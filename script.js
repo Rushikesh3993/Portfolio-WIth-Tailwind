0b6a749e39470e7e9ad6d1d2854a26b2ae853ef9
Rushikesh Mote <moterushikesh39@gmail.com>
2025-07-30 17:06:30 +0530

    enhancedment in education section and some hamburger responsiveness for mobile view

--- script.js
+++ script.js
@@ -3,16 +3,25 @@
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById("close-icon");
+
 //toggle the 'hidden' class on click
 
 menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
+
+    hamburgerIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
 });
 
-//Auto-close on link click
+
+//Auto-close on link click + reset icons
 const menuLinks = mobileMenu.querySelectorAll("a");
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
         mobileMenu.classList.add("hidden");
+        hamburgerIcon.classList.remove("hidden");
+        closeIcon.classList.add("hidden");
     });
 });
